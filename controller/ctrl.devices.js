599070fb289ab898e90208dc47d6bd9b020e7ea2
nwnp <jjw_0418@naver.com>
2022-03-21 17:51:10 +0900

    feat: device CRUD 기능 구현

--- controller/ctrl.devices.js
+++ controller/ctrl.devices.js
@@ -1,6 +1,42 @@
 const Device = require("../models/devices.js");
 
-const getDevices = async (req, res, next) => {};
+const getDevices = async (req, res, next) => {
+  try {
+    const devices = await Device.findAll({});
+    return res.status(200).json({
+      data: {
+        success: true,
+        devices,
+      },
+    });
+  } catch (error) {
+    console.error(`getDevices error: ${error}`);
+  }
+};
+
+const getDevice = async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const exDevice = await Device.findOne({ where: { id } });
+    if (exDevice) {
+      return res.status(200).json({
+        data: {
+          success: true,
+          exDevice,
+        },
+      });
+    } else {
+      return res.status(401).json({
+        data: {
+          success: false,
+          msg: "찾고자 하는 장비가 없음",
+        },
+      });
+    }
+  } catch (error) {
+    console.error(`getDevice error: ${error}`);
+  }
+};
 
 const createDevice = async (req, res, next) => {
   try {
@@ -10,8 +46,9 @@ const createDevice = async (req, res, next) => {
       readyState,
       operatingState,
     });
-    res.status(201).json({
+    return res.status(201).json({
       data: {
+        success: true,
         deviceName,
         readyState,
         operatingState,
@@ -19,10 +56,48 @@ const createDevice = async (req, res, next) => {
     });
   } catch (error) {
     console.error(`createDevice error: ${error}`);
+    return next(error);
+  }
+};
+
+const readyDevice = async (req, res, next) => {
+  const { readyState, id } = req.body;
+  try {
+    await Device.update({ readyState }, { where: { id } });
+    const message = `${!readyState}에서 ${readyState}으로 변경됨`;
+    return res.status(201).json({
+      data: {
+        success: true,
+        msg: message,
+      },
+    });
+  } catch (error) {
+    console.error(`readyDevice error: ${error}`);
+    return next(error);
+  }
+};
+
+const operatingDevice = async (req, res, next) => {
+  const { operatingState, id } = req.body;
+  try {
+    await Device.update({ operatingState }, { where: { id } });
+    const message = `${!operatingState}에서 ${operatingState}으로 변경됨`;
+    return res.status(201).json({
+      data: {
+        success: true,
+        msg: message,
+      },
+    });
+  } catch (error) {
+    console.error(`operatingDevice error: ${error}`);
+    return next(error);
   }
 };
 
 module.exports = {
   getDevices,
+  getDevice,
   createDevice,
+  readyDevice,
+  operatingDevice,
 };
--- routes/devices.js
+++ routes/devices.js
@@ -3,7 +3,19 @@ const router = express.Router();
 
 const ctrlDevices = require("../controller/ctrl.devices.js");
 
+// GET /devices 모든 장비 정보 가져오기
 router.get("/", ctrlDevices.getDevices);
+
+// GET /devices/:id id로 특정 장비만 가져오기
+router.get("/:id", ctrlDevices.getDevice);
+
+// POST /devices/create 장비 만들기
 router.post("/create", ctrlDevices.createDevice);
+
+// PATCH /devices/ready 특정 장비의 준비상태 바꾸기
+router.patch("/ready", ctrlDevices.readyDevice);
+
+// PATCH /devices/operating 특정 장비의 가동상태 바꾸기
+router.patch("/operating", ctrlDevices.operatingDevice);
 
 module.exports = router;
