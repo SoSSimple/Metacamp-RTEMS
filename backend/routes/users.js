05b1b75673bc06ebe37e5abd42b82563b25d3dcc
nwnp <jjw_0418@naver.com>
2022-03-23 21:41:02 +0900

    fix: 일부 수정

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { isLoggedIn, isNotLoggedIn } = require("./middlewares.js");
+// const { isLoggedIn, isNotLoggedIn } = require("./middlewares.js");
 const ctrlUsers = require("../controller/ctrl.users.js");
 
 router.use(ctrlUsers.userSession);
@@ -10,21 +10,21 @@ router.get("/", ctrlUsers.getUsers);
 
 // GET /users/log 유저가 장비를 사용한 이력 가져오기
 // body -> id
-router.get("/log", isLoggedIn, ctrlUsers.getUserLog);
+router.get("/log", ctrlUsers.getUserLog);
 
 // POST /users/signup 회원 가입
-router.post("/signup", isNotLoggedIn, ctrlUsers.signUser);
+router.post("/signup", ctrlUsers.signUser);
 
 // DELETE /users/remove 회원 탈퇴
-router.delete("/remove", isLoggedIn, ctrlUsers.removeUser);
+router.delete("/remove", ctrlUsers.removeUser);
 
 // POST /users/edit/:id 회원 정보 전체 수정
-router.patch("/edit/:id", isLoggedIn, ctrlUsers.editUser);
+router.patch("/edit/:id", ctrlUsers.editUser);
 
 // POST /users/login
-router.post("/login", isNotLoggedIn, ctrlUsers.loginUser);
+router.post("/login", ctrlUsers.loginUser);
 
 // POST /users/logout
-router.post("/logout", isLoggedIn, ctrlUsers.logoutUser);
+router.post("/logout", ctrlUsers.logoutUser);
 
 module.exports = router;
