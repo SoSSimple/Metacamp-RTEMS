d52f9719103d25b6b7909d1790e7a2e4550e575f
huiyeongyoon <61744926+huiyeongyoon@users.noreply.github.com>
2022-03-25 10:36:23 +0900

    feat : login and signup

---
+++ front/layouts/default.vue
@@ -0,0 +1,115 @@
+<template>
+  <v-app>
+    <nav v-if="!me">
+      <v-toolbar dark color="black">
+        <v-toolbar-title>
+          <nuxt-link to="/">메인페이지</nuxt-link>
+        </v-toolbar-title>
+        <v-spacer />
+        <v-toolbar-items>
+          <v-text-field label="검색" hide-details="" prepend-icon="mdi-magnify" :style="{ display: 'flex', alignItems: 'center'}" />
+          <v-btn text nuxt to="/signup" :style="{ display: 'flex', alignItems: 'center'}">
+            <div>회원가입</div>
+          </v-btn>
+          <v-btn text nuxt to="/login" :style="{ display: 'flex', alignItems: 'center'}">
+            <div>로그인</div>
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+    </nav>
+    <nav v-else>
+      <v-toolbar dark color="black">
+        <v-toolbar-title>
+          <nuxt-link to="/">메인페이지</nuxt-link>
+        </v-toolbar-title>
+        <v-spacer />
+        <v-toolbar-items>
+          <v-text-field label="검색" hide-details="" prepend-icon="mdi-magnify" :style="{ display: 'flex', alignItems: 'center'}" />
+          <v-btn text nuxt to="/profile" :style="{ display: 'flex', alignItems: 'center'}">
+            <div>마이페이지</div>
+          </v-btn>
+          {{me.name}} 님 안녕하세요
+          <v-btn text nuxt to="/logout" :style="{ display: 'flex', alignItems: 'center'}">
+            <div @click="onLogOut">로그아웃</div>
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+    </nav>
+    <v-row>
+      <!-- <v-col cols="12" md="2">
+        <v-sheet
+          height="100vh"
+          class="overflow-hidden"
+          style="position: relative;"
+        >
+          <v-container class="fill-height">
+            <v-row
+              :style="{display: 'flex'}"
+            >
+              <v-btn
+                color="pink"
+                dark
+                @click.stop="drawer = !drawer"
+              >
+                Toggle
+              </v-btn>
+            </v-row>
+          </v-container>
+
+          <v-navigation-drawer
+            v-model="drawer"
+            absolute
+            temporary
+          >
+            <v-list dense>
+              <v-list-item
+                v-for="item in items"
+                :key="item.title"
+                link
+              >
+                <v-list-item-content>
+                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-navigation-drawer>
+        </v-sheet>
+      </v-col>
+      <v-col cols="12" md="10"> -->
+      <nuxt />
+      <!-- </v-col> -->
+    </v-row>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+        return {
+          drawer: null,
+          items: [
+            { title: 'Home', icon: 'mdi-view-dashboard' },
+            { title: 'About', icon: 'mdi-forum' },
+          ],
+        }
+      },
+    computed: {
+      me() {
+        return this.$store.state.users.me
+      }
+    },
+    methods: {
+      onLogOut() {
+        this.$store.dispatch('users/logOut')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  a {
+    display: inline-block;
+    text-decoration: none;
+    color: inherit;
+  }
+</style>
---
+++ front/pages/signUp.vue
@@ -0,0 +1,111 @@
+<template>
+  <div>
+    <v-container :style="{ width: '100vw', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}">
+      <v-card :style="{ width: '500px'}">
+        <v-container>
+          <v-subheader>회원가입</v-subheader>
+          <v-form ref="form" v-model="valid" @submit.prevent="onSubmitForm">
+            <v-container>
+              <v-text-field
+                v-model="userId"
+                label="아이디"
+                type="email"
+                :rules="userIdRules"
+                required
+              />
+              <v-text-field
+                v-model="password"
+                label="비밀번호"
+                type="password"
+                :rules="passwordRules"
+                required
+              />
+              <v-text-field
+                v-model="name"
+                label="이름"
+                type="password"
+                :rules="nameRules"
+                required
+              />
+              <v-select
+                :items="department"
+                label="부서"
+                :rules="departmentRules"
+                solo
+              />
+              <v-select
+                :items="role"
+                label="역할"
+                :rules="roleRules"
+                solo
+              />
+              <v-container :style="{ display: 'flex', justifyContent:'flex-end'}">
+                <v-btn type="submit">가입하기</v-btn>
+              </v-container>
+              <v-container />
+            </v-container>
+          </v-form>
+        </v-container>
+      </v-card>
+    </v-container>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        valid: false,
+        userId: '',
+        password: '',
+        name: '',
+        department: ['dev','sales'],
+        role: ['worker','admin'],
+        userIdRules: [
+          v => !!v || '이메일은 필수입니다.',
+          v => /.+@.+/.test(v) || '이메일이 유효하지 않습니다.',
+        ],
+        passwordRules: [
+          v => !!v || '비밀번호는 필수입니다.',
+        ],
+        nameRules: [
+          v => !!v || '이름은 필수입니다.',
+        ],
+        departmentRules: [
+          v => !!v || '부서선택은 필수입니다.',
+        ],
+        roleRules: [
+          v => !!v || '역할선택은 필수입니다.',
+        ],
+      };
+    },
+    head() {
+      return {
+        title: '회원가입',
+      };
+    },
+    methods: {
+      onSubmitForm() {
+        if (this.$refs.form.validate()) {
+          // actions dispatch
+          this.$store.dispatch('users/signUp', {
+            name: this.name,
+            userId: this.userId
+          })
+          .then(() => {
+              this.$router.push({
+                path: '/'
+              })
+            })
+            .catch(() => {
+              alert('회원가입 실패')
+            })  
+        } 
+      }
+    },
+  };
+</script>
+
+<style>
+
+</style>
