5124956fd0cfdb5fe3a0266a9d185b01db4bdaa0
nwnp-laptop <jjw_0418@naver.com>
2022-03-31 12:47:36 +0900

    feat: frontend - initial setting

---
+++ frontend/src/components/layout/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <hr />
+    <span>This is Footer</span>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ frontend/src/components/layout/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>This is Header</h1>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
